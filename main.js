b1abd62550209cdc7f3f5d5342d4c1ceb79ad027
Matheus Santos <matheusps0012@gmail.com>
2021-01-08 09:24:36 -0300

    Done - encapsulado dentro de um metodo

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 const fs = require('fs')
 
-fs.readFile('test.json' , (err, rawdata)=> {
-const data = JSON.parse(rawdata)
+function convertFile(file){
+  fs.readFile(file , (err, rawdata)=> {
+  const data = JSON.parse(rawdata)
 
-const replacer = (key, value) => value === null ? '' : value
-const header = Object.keys(data[0])
-const csv = [
-  header.join(','),
-  ...data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-].join('\r\n')
+  const replacer = (key, value) => value === null ? '' : value
+  const header = Object.keys(data[0])
+  const csv = [
+    header.join(';'),
+    ...data.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(';'))
+  ].join('\r\n')
 
-fs.writeFile('test.csv', csv, (err) => {
+  fs.writeFile(`${file}.csv`, csv, (err) => {
     if(err) {
         throw err
     }
-})
+  })
 
 })
+}
+
+convertFile('Proffuzzi2020-10-11.json')
